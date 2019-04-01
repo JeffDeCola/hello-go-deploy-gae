966346bb32a41f6829faa7289db6f7a2705dfe66
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-01 01:49:52 -0700

    example 2

--- example-01-app/deploy-gae.sh
+++ example-01-app/deploy-gae.sh
@@ -6,6 +6,10 @@ echo " "
 
 echo "DEPLOY APP TO GAE"
 echo " "
+echo "      gcloud app browse"
+echo "      gcloud app logs tail -s example-01-app"
+echo "      gcloud app browse -s example-01-app"
+echo " "
 gcloud app deploy app.yaml
 echo " "
 
