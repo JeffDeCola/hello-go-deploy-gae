9ae63ba2a3c32cde63729304f0de68073f449825
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-02 09:12:11 -0700

    finished example 3

--- example-03-app/main.go
+++ example-03-app/main.go
@@ -5,36 +5,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"google.golang.org/appengine"
 )
 
-// Addthis Adds two ints together
-func addThis(a int, b int) (temp int) {
-	temp = a + b
-	return
+// Counter is the sum and mutex.
+type Counter struct {
+	sum  int
+	lock sync.RWMutex
+}
+
+var counter = &Counter{}
+
+// addThis adds
+func (c *Counter) addThis(i int) {
+	c.lock.Lock()
+	c.sum = c.sum + i
+	c.lock.Unlock()
 }
 
 func loopForever() {
-	var a = 0
-	var b = 1
+	counter.sum = 5
+	var i = 1
 	for true {
-		a = addThis(a, b)
-		fmt.Println("Hello all, I finished this!! The count is:", a)
+		counter.addThis(i)
+		fmt.Println("Jeff - The count is:", counter.sum)
 		time.Sleep(2000 * time.Millisecond)
 	}
 }
 
 func main() {
 
-	loopForever()
+	go loopForever()
 
 	http.HandleFunc("/", handleJeff)
 	appengine.Main()
+	// log.Fatal(http.ListenAndServe(":1234", nil))
+
 }
 
 func handleJeff(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world! - Example 03")
-	fmt.Fprintln(w, "The current number is 7")
+	fmt.Fprintln(w, "The current count is", counter.sum)
 }
