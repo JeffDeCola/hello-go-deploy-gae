df9d0e3ada3b41d7b5df3e8e2cba43d88bb44154
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-05 04:26:26 -0700

    cleaned up scripts

--- example-02-gae/deploy-gae.sh
+++ example-02-gae/deploy-gae.sh
@@ -1,23 +1,33 @@
-#!/bin/sh
+#!/bin/sh -e 
 # hello-go-deploy-gae deploy-gae.sh
 
-echo "deploy-gae.sh (END)"
 echo " "
 
-echo "Sync contents of bucket on google cloud storage with /bucket-gcs"
-# gsutil mb -l us-west1 gs://example-02-app/
-# gsutil defacl set public-read gs://example-02-app
-gsutil -m rsync -r ./bucket-gcs gs://example-02-app/b
-echo " "
+if [ "$1" = "-debug" ]
+then
+    echo "************************************************************************"
+    echo "* deploy-gae.sh -debug (START) *****************************************"
+    echo "************************************************************************"
+    # set -x enables a mode of the shell where all executed commands are printed to the terminal.
+    set -x
+    echo " "
+else
+    echo "************************************************************************"
+    echo "* deploy-gae.sh (START) ************************************************"
+    echo "************************************************************************"
+    echo " "
+fi
 
 echo "DEPLOY APP TO GAE"
 echo " "
 echo "      gcloud app browse"
-echo "      gcloud app logs tail -s example-02-app"
-echo "      gcloud app browse -s example-02-app"
+echo "      gcloud app logs tail -s example-01-app"
+echo "      gcloud app browse -s example-01-app"
 echo " "
 gcloud app deploy app.yaml
 echo " "
 
-echo "deploy-gae.sh (END)"
+echo "************************************************************************"
+echo "* deploy-gae.sh (END) **************************************************"
+echo "************************************************************************"
 echo " "
