f818e28282bf6d6612ac16d26b1a0303aaa8f6a0
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-07 06:31:14 -0700

    why is /mnt/c/Users/Jeff/home/jeff /go and not /root

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -17,6 +17,8 @@ else
     echo " "
 fi
 
+echo "************************************************ gopath is: $GOPATH"
+
 echo "The goal is to set up a go src/github.com/JeffDeCola/hello-go-deploy-gae directory"
 echo "Then tests will be run in that directory"
 echo "Test coverage results, text_coverage.txt, will be moved to /coverage-results directory"
